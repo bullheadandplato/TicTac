70d131a8c6f2a36c3c7e4ed28dc76795890800e5
Osama <m.osamabinomar@gmail.com>
2016-05-11 21:33:08 +0500

    this is first commit.

---
+++ src/com/osama/frontend/MainViewController.java
@@ -0,0 +1,17 @@
+package com.osama.frontend;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by osama on 5/11/16.
+ * This file will control the interface
+ */
+public class MainViewController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
