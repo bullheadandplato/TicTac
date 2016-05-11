1920fd90604a6e9882bf763e531a4b018d9ca237
Osama <m.osamabinomar@gmail.com>
2016-05-12 00:04:02 +0500

    Interface is almost complete

---
+++ src/com/osama/backend/Interface/Drawer.java
@@ -0,0 +1,84 @@
+package com.osama.backend.Interface;
+
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
+/**
+ * Created by osama on 5/11/16.
+ */
+public class Drawer  {
+    private Canvas canvas;
+    private GraphicsContext graphicsContext;
+    private static final int w=30;
+    private static final int h=30;
+    private  Image image;
+
+
+    public Canvas drawGame(){
+        canvas =new Canvas(200,200);
+        graphicsContext=canvas.getGraphicsContext2D();
+        drawLines(graphicsContext);
+        return canvas;
+
+    }
+    private void drawLines(GraphicsContext gc) {
+        gc.setLineWidth(5);
+        gc.setStroke(Color.CYAN);
+        gc.beginPath();
+        gc.strokeLine(0,40,190,30);
+        gc.strokeLine(40,0,30,190);
+        gc.strokeLine(0,130,190,120);
+        gc.strokeLine(130,0,120,190);
+        gc.stroke();
+    }
+    public void drawImage(int box,int player) {
+        if(player==1){
+             image = new Image("images/cross.png");
+        }
+        else if(player==2){
+            image=new Image("images/zero.png");
+        }
+        switch (box){
+            case 1:{
+                graphicsContext.drawImage(image, 5, 5, w, h);
+                break;
+            }
+            case 2:{
+                graphicsContext.drawImage(image,0,70,w,h);
+                break;
+            }
+            case 3:{
+                graphicsContext.drawImage(image,0,150,w,h);
+                break;
+            }
+            case 4:{
+                graphicsContext.drawImage(image,70,2,w,h);
+                break;
+            }
+            case 5:{
+                graphicsContext.drawImage(image,70,70,w,h);
+                break;
+            }
+            case 6:{
+                graphicsContext.drawImage(image,70,150,w,h);
+                break;
+            }
+            case 7:{
+                graphicsContext.drawImage(image,150,0,w,h);
+                break;
+            }
+            case 8:{
+                graphicsContext.drawImage(image,150,70,w,h);
+                break;
+            }
+            case 9:{
+                graphicsContext.drawImage(image,150,150,w,h);
+                break;
+            }
+        }
+    }
+
+}
--- src/com/osama/frontend/MainViewController.java
+++ src/com/osama/frontend/MainViewController.java
@@ -1,6 +1,12 @@
 package com.osama.frontend;
 
+import com.osama.backend.Interface.Drawer;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.HBox;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,8 +16,68 @@ import java.util.ResourceBundle;
  * This file will control the interface
  */
 public class MainViewController implements Initializable {
+    @FXML
+    private HBox box;
+    private Canvas canvas;
+    private Drawer drawer;
+    private int clickedX;
+    private int clickedY;
+    private static final int Box1X = 40;
+    private static final int Box1Y = 40;
+    private static final int Box2Y = 130;
+    private static final int Box3Y = 190;
+    private static final int Box2X = 130;
+    private static final int Box3X = 190;
+    private static int player=0;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        drawer = new Drawer();
+        canvas = drawer.drawGame();
+        box.getChildren().addAll(canvas);
+        canvas.setOnMouseClicked(event -> {
+            Platform.runLater(() -> {
+                clickedX = (int) event.getX();
+                clickedY = (int) event.getY();
+                if (clickedX < Box1X && clickedY < Box1Y) {
 
+                    drawer.drawImage(1,togglePlayer());
+                } else if (clickedX < Box1X && clickedY < Box2Y) {
+                    drawer.drawImage(2,togglePlayer());
+
+                } else if (clickedX < Box1X && clickedY < Box3Y) {
+                    drawer.drawImage(3,togglePlayer());
+
+                } else if (clickedX < Box2X && clickedY < Box1Y) {
+                    drawer.drawImage(4,togglePlayer());
+
+                } else if (clickedX < Box2X && clickedY < Box2Y) {
+                    drawer.drawImage(5,togglePlayer());
+                } else if (clickedX < Box2X && clickedY < Box3Y) {
+                    drawer.drawImage(6,togglePlayer());
+                } else if (clickedX < Box3X && clickedY < Box1Y) {
+                    drawer.drawImage(7,togglePlayer());
+                } else if (clickedX < Box3X && clickedY < Box2Y) {
+                    drawer.drawImage(8,togglePlayer());
+                } else if (clickedX < Box3X && clickedY < Box3Y) {
+                    drawer.drawImage(9,togglePlayer());
+                }
+
+            });
+
+
+        });
+    }
+    private int togglePlayer(){
+        if(player==0){
+            player=1;
+        }
+        else if(player==1){
+            player=2;
+        }
+        else if(player==2){
+            player=1;
+        }
+        return player;
     }
 }
