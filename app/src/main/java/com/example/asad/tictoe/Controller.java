3a258ac44dc9ce9d57c88025145b47c12db3dc49
Ranaasad113 <ranaasad737@gmail.com>
2016-05-29 22:05:14 +0500

    Almost Complete basic things

--- app/src/main/java/com/example/asad/tictoe/Controller.java
+++ app/src/main/java/com/example/asad/tictoe/Controller.java
@@ -9,7 +9,7 @@ import android.view.MotionEvent;
 public class Controller {
     private static Drawer instace1;
     private float  x_axis,y_axis;
-    private int[][] WinBox=new int[][]{{0,0,0},{0,0,0},{0,0,0}};
+    private int[][] WinBox=new int[][]{{0,0,0},{0,0,0},{0,0,0}};//This save the box which is click
     private int id=1;
 
     public Controller()
@@ -19,161 +19,187 @@ public class Controller {
         instace1=canvas;
 
     }
-    public void Boxtrack(float x,float y)//This give the value to array e.g if first box is touch then arr[0]=1
-    { x_axis=x;
-        y_axis=y;
-        if (instace1.thirdcord < x_axis && x_axis < instace1.firstcord &&instace1.fifthcord < y_axis && y_axis <instace1.sixcord)//For A11 Box
-        {   if(id%2!=0)
-        {  instace1.arr2[0]=1;
-            WinBox[0][0]=1;
-            id++;
-        }
-            else {
-            instace1.arr2[0]=2;
-            WinBox[0][0] = 2;
-            id++;
-        }
-            instace1.arr[0] = 1;
-        }
-        else if (instace1.firstcord < x_axis && x_axis < instace1.secndcord && instace1.fifthcord < y_axis && y_axis < instace1.sixcord) {
-            if(id%2!=0)
-            {instace1.arr2[1]=1;
-                WinBox[0][1]=1;
-                id++;
+    public void Boxtrack(float x,float y)//This give the value to array e.g if first box is touch then arr[0]=1 and also player @id
+    {
+        x_axis = x;
+        y_axis = y;
+
+        if (instace1.thirdcord < x_axis && x_axis < instace1.firstcord && instace1.fifthcord < y_axis && y_axis < instace1.sixcord)//For A11 Box
+        {
+            if (instace1.arr[0] == 0) {
+                if (id % 2 != 0) {
+                    instace1.arr2[0] = 1;
+                    WinBox[0][0] = 1;
+                    id++;
+                } else {
+                    instace1.arr2[0] = 2;
+                    WinBox[0][0] = 2;
+                    id++;
+                }
+                instace1.arr[0] = 1;
             }
-            else {instace1.arr2[1]=2;
-                WinBox[0][1] = 2;
-                id++;
+        } else if (instace1.firstcord < x_axis && x_axis < instace1.secndcord && instace1.fifthcord < y_axis && y_axis < instace1.sixcord) {
+
+            if (instace1.arr[1] == 0) {
+                if (id % 2 != 0) {
+                    instace1.arr2[1] = 1;
+                    WinBox[0][1] = 1;
+                    id++;
+                } else {
+                    instace1.arr2[1] = 2;
+                    WinBox[0][1] = 2;
+                    id++;
+                }
+                instace1.arr[1] = 2;
             }
-            instace1.arr[1] = 2;
-        }
-        else if (instace1.secndcord < x_axis && x_axis < instace1.forthcord && instace1.fifthcord < y_axis && y_axis < instace1.sixcord)
-        {if(id%2!=0) {
-            instace1.arr2[2]=1;
-            WinBox[0][2] = 1;
-            id++;
-        }
-        else {
-            instace1.arr2[2]=2;
-            WinBox[0][2] = 2;
-            id++;
-        }
-            instace1.arr[2] = 3;}
-        else if (instace1.thirdcord < x_axis && x_axis < instace1.firstcord && instace1.sixcord < y_axis && y_axis < instace1.sevencord) {
-            if(id%2!=0) {
-             instace1.arr2[3]=1;
-                WinBox[1][0] = 1;
-                id++;
+        } else if (instace1.secndcord < x_axis && x_axis < instace1.forthcord && instace1.fifthcord < y_axis && y_axis < instace1.sixcord) {
+            if (instace1.arr[2] == 0) {
+                if (id % 2 != 0) {
+                    instace1.arr2[2] = 1;
+                    WinBox[0][2] = 1;
+                    id++;
+                } else {
+                    instace1.arr2[2] = 2;
+                    WinBox[0][2] = 2;
+                    id++;
+                }
+                instace1.arr[2] = 3;
             }
-            else {
-                instace1.arr2[3]=2;
-                WinBox[1][0] = 2;
-                id++;
+        } else if (instace1.thirdcord < x_axis && x_axis < instace1.firstcord && instace1.sixcord < y_axis && y_axis < instace1.sevencord) {
+            if (instace1.arr[3] == 0) {
+                if (id % 2 != 0) {
+                    instace1.arr2[3] = 1;
+                    WinBox[1][0] = 1;
+                    id++;
+                } else {
+                    instace1.arr2[3] = 2;
+                    WinBox[1][0] = 2;
+                    id++;
+                }
+
+                instace1.arr[3] = 4;
             }
-            instace1.arr[3] = 4;
-        }
-        else if (instace1.firstcord < x_axis && x_axis < instace1.secndcord && instace1.sixcord < y_axis && y_axis < instace1.sevencord) {
-            if(id%2!=0) {
-                instace1.arr2[4]=1;
-                WinBox[1][1] = 1;
-                id++;
+        } else if (instace1.firstcord < x_axis && x_axis < instace1.secndcord && instace1.sixcord < y_axis && y_axis < instace1.sevencord) {
+            if (instace1.arr[4] == 0) {
+                if (id % 2 != 0) {
+                    instace1.arr2[4] = 1;
+                    WinBox[1][1] = 1;
+                    id++;
+                } else {
+                    instace1.arr2[4] = 2;
+                    WinBox[1][1] = 2;
+                    id++;
+                }
+                instace1.arr[4] = 5;
             }
-            else {
-                instace1.arr2[4]=2;
-                WinBox[1][1] = 2;
-                id++;
+        } else if (instace1.secndcord < x_axis && x_axis < instace1.forthcord && instace1.sixcord < y_axis && y_axis < instace1.sevencord) {
+            if (instace1.arr[5] == 0) {
+                if (id % 2 != 0) {
+                    instace1.arr2[5] = 1;
+                    WinBox[1][2] = 1;
+                    id++;
+                } else {
+                    instace1.arr2[5] = 2;
+                    WinBox[1][2] = 2;
+                    id++;
+                }
+                instace1.arr[5] = 6;
             }
-            instace1.arr[4] = 5;
-        }
-        else if (instace1.secndcord < x_axis && x_axis < instace1.forthcord && instace1.sixcord < y_axis && y_axis < instace1.sevencord) {
-            if(id%2!=0) {
-                instace1.arr2[5]=1;
-                WinBox[1][2] = 1;
-                id++;
+        } else if (instace1.thirdcord < x_axis && x_axis < instace1.firstcord && instace1.sevencord < y_axis && y_axis < instace1.eightcord) {
+            if (instace1.arr[6] == 0) {
+                if (id % 2 != 0) {
+                    instace1.arr2[6] = 1;
+                    WinBox[2][0] = 1;
+                    id++;
+                } else {
+                    instace1.arr2[6] = 2;
+                    WinBox[2][0] = 2;
+                    id++;
+                }
+                instace1.arr[6] = 7;
             }
-            else {
-                instace1.arr2[5]=2;
-                WinBox[1][2] = 2;
-                id++;
+        } else if (instace1.firstcord < x_axis && x_axis < instace1.secndcord && instace1.sevencord < y_axis && y_axis < instace1.eightcord) {
+            if (instace1.arr[7] == 0) {
+                if (id % 2 != 0) {
+                    instace1.arr2[7] = 1;
+                    WinBox[2][1] = 1;
+                    id++;
+                } else {
+                    instace1.arr2[7] = 2;
+                    WinBox[2][1] = 2;
+                    id++;
+                }
+                instace1.arr[7] = 8;
             }
-            instace1.arr[5] = 6;
-        }
-        else if (instace1.thirdcord < x_axis && x_axis < instace1.firstcord && instace1.sevencord < y_axis && y_axis < instace1.eightcord) {
-            if(id%2!=0) {
-                instace1.arr2[6]=1;
-                WinBox[2][0] = 1;
-                id++;
-            }
-            else {
-                instace1.arr2[6]=2;
-                WinBox[2][0] = 2;
-                id++;
-            }
-            instace1.arr[6] = 7;
-        }
-        else if (instace1.firstcord < x_axis && x_axis < instace1.secndcord && instace1.sevencord < y_axis && y_axis < instace1.eightcord) {
-            if(id%2!=0) {
-                instace1.arr2[7]=1;
-                WinBox[2][1] = 1;
-                id++;
-            }
-            else {
-                instace1.arr2[7]=2;
-                WinBox[2][1] = 2;
-                id++;
-            }
-            instace1.arr[7] = 8;
-        }
-        else if (instace1.secndcord < x_axis && x_axis < instace1.forthcord && instace1.sevencord < y_axis && y_axis < instace1.eightcord) {
-            if(id%2!=0) {
-                instace1.arr2[8]=1;
-                WinBox[2][2] = 1;
-                id++;
-            }
-            else {
-                instace1.arr2[8]=2;
-                WinBox[2][2] = 2;
-                id++;
-            }
+        } else if (instace1.secndcord < x_axis && x_axis < instace1.forthcord && instace1.sevencord < y_axis && y_axis < instace1.eightcord) {
+            if (instace1.arr[8] == 0){
+                if (id % 2 != 0) {
+                    instace1.arr2[8] = 1;
+                    WinBox[2][2] = 1;
+                    id++;
+                } else {
+                    instace1.arr2[8] = 2;
+                    WinBox[2][2] = 2;
+                    id++;
+                }
             instace1.arr[8] = 9;
         }
+    }
 
 
     }
-    public  void Check_Condition()
-    {
-        if(WinBox[0][0]==2&&WinBox[1][0]==2&&WinBox[2][0]==2)
+    public  boolean Check_Condition() {
+        if (WinBox[0][0] == 2 && WinBox[1][0] == 2 && WinBox[2][0] == 2) {
             instace1.Coulom_First();
-        else if(WinBox[0][1]==2&&WinBox[1][1]==2&&WinBox[2][1]==2)
+            return true;
+        } else if (WinBox[0][1] == 2 && WinBox[1][1] == 2 && WinBox[2][1] == 2) {
             instace1.Coulom_Two();
-        else if(WinBox[0][2]==2&&WinBox[1][2]==2&&WinBox[2][2]==2)
+            return true;
+        } else if (WinBox[0][2] == 2 && WinBox[1][2] == 2 && WinBox[2][2] == 2) {
             instace1.Coulom_Three();
-        else if(WinBox[0][0]==2&&WinBox[0][1]==2&&WinBox[0][2]==2)
+            return true;
+        } else if (WinBox[0][0] == 2 && WinBox[0][1] == 2 && WinBox[0][2] == 2) {
             instace1.Row_One();
-        else if(WinBox[1][0]==2&&WinBox[1][1]==2&&WinBox[1][2]==2)
+            return true;
+        } else if (WinBox[1][0] == 2 && WinBox[1][1] == 2 && WinBox[1][2] == 2) {
             instace1.Row_Two();
-        else if(WinBox[2][0]==2&&WinBox[2][1]==2&&WinBox[2][2]==2)
+            return true;
+        } else if (WinBox[2][0] == 2 && WinBox[2][1] == 2 && WinBox[2][2] == 2) {
             instace1.Row_Three();
-        else if (WinBox[0][0]==2&&WinBox[1][1]==2&&WinBox[2][2]==2)
+            return true;
+        } else if (WinBox[0][0] == 2 && WinBox[1][1] == 2 && WinBox[2][2] == 2) {
             instace1.Daigonal_One();
-        else if (WinBox[0][2]==2&&WinBox[1][1]==2&&WinBox[2][0]==2)
+            return true;
+        } else if (WinBox[0][2] == 2 && WinBox[1][1] == 2 && WinBox[2][0] == 2) {
             instace1.Daigonal_Two();
-        if(WinBox[0][0]==1&&WinBox[1][0]==1&&WinBox[2][0]==1)
+            return true;
+        }
+        if (WinBox[0][0] == 1 && WinBox[1][0] == 1 && WinBox[2][0] == 1) {
             instace1.Coulom_First();
-    else if(WinBox[0][1]==1&&WinBox[1][1]==1&&WinBox[2][1]==1)
+            return true;
+        } else if (WinBox[0][1] == 1 && WinBox[1][1] == 1 && WinBox[2][1] == 1) {
             instace1.Coulom_Two();
-        else if(WinBox[0][2]==1&&WinBox[1][2]==1&&WinBox[2][2]==1)
+            return true;
+        } else if (WinBox[0][2] == 1 && WinBox[1][2] == 1 && WinBox[2][2] == 1) {
             instace1.Coulom_Three();
-        else if(WinBox[0][0]==1&&WinBox[0][1]==1&&WinBox[0][2]==1)
+            return true;
+        } else if (WinBox[0][0] == 1 && WinBox[0][1] == 1 && WinBox[0][2] == 1) {
             instace1.Row_One();
-        else if(WinBox[1][0]==1&&WinBox[1][1]==1&&WinBox[1][2]==1)
+            return true;
+        } else if (WinBox[1][0] == 1 && WinBox[1][1] == 1 && WinBox[1][2] == 1) {
             instace1.Row_Two();
-        else if(WinBox[2][0]==1&&WinBox[2][1]==1&&WinBox[2][2]==1)
+            return true;
+        } else if (WinBox[2][0] == 1 && WinBox[2][1] == 1 && WinBox[2][2] == 1) {
             instace1.Row_Three();
-        else if (WinBox[0][0]==1&&WinBox[1][1]==1&&WinBox[2][2]==1)
+            return true;
+        } else if (WinBox[0][0] == 1 && WinBox[1][1] == 1 && WinBox[2][2] == 1) {
             instace1.Daigonal_One();
-        else if (WinBox[0][2]==1&&WinBox[1][1]==1&&WinBox[2][0]==1)
+            return true;
+        } else if (WinBox[0][2] == 1 && WinBox[1][1] == 1 && WinBox[2][0] == 1) {
             instace1.Daigonal_Two();
+            return true;
+        }
+        return false;
     }
 
 }
--- app/src/main/java/com/example/asad/tictoe/Drawer.java
+++ app/src/main/java/com/example/asad/tictoe/Drawer.java
@@ -18,7 +18,10 @@ import android.widget.Toast;
 /**
  * Created by asad on 5/25/16.
  */
-public class Drawer extends View {
+public class Drawer extends View {/*
+Drawer Class draw the whole UI and placing image in the boxes and
+ also draw the lines when Player is won
+*/
     public Canvas canvas1 = new Canvas();
     public float x_axis, y_axis;
     int height, width;
@@ -26,6 +29,7 @@ public class Drawer extends View {
     Bitmap mFinalbitmap2=BitmapFactory.decodeResource(getResources(), R.drawable.zero);
     public int[] arr = new int[9];
     private Controller controller=new Controller();
+    //These are the coordinate of the screen cord is coordinate
     public float firstcord = 0f;
     public float secndcord = 0f;
     public float thirdcord = 0f;
@@ -34,11 +38,11 @@ public class Drawer extends View {
     public float sixcord = 0f;
     public float sevencord = 0f;
     public float eightcord = 0f;
-    public float widthborder = 0f;
+    public float widthborder = 0f;//Width of the lines
     public  int[] arr2=new int[9];
     public float h = getContext().getResources().getDisplayMetrics().heightPixels;
-    public float w = getContext().getResources().getDisplayMetrics().widthPixels;
-
+    public float w = getContext().getResources().getDisplayMetrics().widthPixels;//This get the total height and width of screen
+    private boolean getvalue=false;
     public Drawer(Context context) {
         super(context);
 
@@ -53,8 +57,8 @@ public class Drawer extends View {
         eightcord = h / 1.28f;
         width = (int) (firstcord - (thirdcord + 25));
         height = (int) (sixcord - (fifthcord + 25));
-        mFinalbitmap = Bitmap.createScaledBitmap(mFinalbitmap, width, height, false);
-        mFinalbitmap2 = Bitmap.createScaledBitmap(mFinalbitmap2, width, height, false);
+        mFinalbitmap = Bitmap.createScaledBitmap(mFinalbitmap, width, height, false);//Set width and Height of image
+        mFinalbitmap2 = Bitmap.createScaledBitmap(mFinalbitmap2, width, height, false);//Set width and Height of image
     }
 
     @Override
@@ -78,84 +82,87 @@ public class Drawer extends View {
     public boolean onTouchEvent(MotionEvent ev) {
         x_axis = ev.getX();
         y_axis = ev.getY();
-        Log.i("Tag", "Rana asad");
         switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN://avc
-                controller.Boxtrack(x_axis,y_axis);
-                invalidate();
-                break;
+            case MotionEvent.ACTION_DOWN:
+                /*When Touch the Box and anywhere on the screen then x_axis an y-axis location is
+                 pass to Boxtrack which is the method of #Controller Class.
+                  */
+                if (getvalue==false)
+                {controller.Boxtrack(x_axis,y_axis);
+                invalidate();//This Redraw the whole Graphics from starting
+                break;}
         }
 
 
         return true;
     }
 
-    public void ShowimageA11() {
+    public void ShowimageA11() {//This Method show the image for First one Box
 
         if (arr2[0]==1)
         canvas1.drawBitmap(mFinalbitmap, thirdcord, fifthcord, null);
         else if (arr2[0]==2)
         canvas1.drawBitmap(mFinalbitmap2, thirdcord, fifthcord, null);
-        controller.Check_Condition();
+        getvalue=controller.Check_Condition();
     }
 
-    public void ShowimageA12() {
+    public void ShowimageA12() {//This Method show the image for Second Box
         if (arr2[1]==1)
         canvas1.drawBitmap(mFinalbitmap, (firstcord+25), fifthcord, null);
         else if (arr2[1]==2)
         canvas1.drawBitmap(mFinalbitmap2, (firstcord+25), fifthcord, null);
-        controller.Check_Condition();
+        getvalue=controller.Check_Condition();
     }
-    public void ShowImageA13() {
+    public void ShowImageA13() {//This Mathod show the image for Third Box
         if (arr2[2]==1)
         canvas1.drawBitmap(mFinalbitmap, (secndcord+25), fifthcord, null);
         else if (arr2[2]==2)
         canvas1.drawBitmap(mFinalbitmap2, (secndcord+25), fifthcord, null);
-        controller.Check_Condition();
+        getvalue=controller.Check_Condition();
     }
-    public  void ShowImageA21(){
+    public  void ShowImageA21(){//This Method show the image for Forth Box
         if (arr2[3]==1)
         canvas1.drawBitmap(mFinalbitmap, (thirdcord), (sixcord+45), null);
         else if (arr2[3]==2)
         canvas1.drawBitmap(mFinalbitmap2, (thirdcord), (sixcord+45), null);
-        controller.Check_Condition();
+        getvalue=controller.Check_Condition();
     }
-    public  void ShowImageA22(){
+    public  void ShowImageA22(){//This Method show the image for Fifth Box
         if (arr2[4]==1)
         canvas1.drawBitmap(mFinalbitmap, (firstcord+25), (sixcord+45), null);
         else if (arr2[4]==2)
         canvas1.drawBitmap(mFinalbitmap2, (firstcord+25), (sixcord+45), null);
-        controller.Check_Condition();
+        getvalue=controller.Check_Condition();
     }
-    public  void ShowImageA23(){
+    public  void ShowImageA23(){//This Method show the image for Sixth Box
         if (arr2[5]==1)
         canvas1.drawBitmap(mFinalbitmap, (secndcord+25), (sixcord+45), null);
         else if (arr2[5]==2)
         canvas1.drawBitmap(mFinalbitmap2, (secndcord+25), (sixcord+45), null);
-        controller.Check_Condition();
+        getvalue=controller.Check_Condition();
     }
 
-    public  void ShowImageA31(){
+    public  void ShowImageA31(){//This Method show the image for Seventh Box
         if (arr2[6]==1)
         canvas1.drawBitmap(mFinalbitmap, (thirdcord), (sevencord+25), null);
         else if (arr2[6]==2)
         canvas1.drawBitmap(mFinalbitmap2, (thirdcord), (sevencord+25), null);
-        controller.Check_Condition();
+        getvalue=controller.Check_Condition();
 
     }
-    public void ShowImmageA32() {
+    public void ShowImmageA32() {//This Method show the image for Eighth Box
         if (arr2[7]==1)
         canvas1.drawBitmap(mFinalbitmap, (firstcord+25), (sevencord+25), null);
         else if(arr2[7]==2)
         canvas1.drawBitmap(mFinalbitmap2, (firstcord+25), (sevencord+25), null);
-        controller.Check_Condition();
+        getvalue=controller.Check_Condition();
     }
-    public void ShowImageA33(){
+    public void ShowImageA33(){//This Method show the image for Ninth Box
         if (arr2[8]==1)
         canvas1.drawBitmap(mFinalbitmap, (secndcord+25), (sevencord+25), null);
         else  if (arr2[8]==2)
         canvas1.drawBitmap(mFinalbitmap2, (secndcord+25), (sevencord+25), null);
-        controller.Check_Condition();
+        getvalue=controller.Check_Condition();
     }
 
 
