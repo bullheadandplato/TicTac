9236fabb8d1f1e611f1aafbae24cdffb87e6ae17
Osama <m.osamabinomar@gmail.com>
2016-09-19 22:45:13 +0500

    new code

--- app/src/main/java/com/osama/backend/UIController.java
+++ app/src/main/java/com/osama/backend/UIController.java
@@ -2,6 +2,8 @@ package com.osama.backend;
 
 import android.app.Activity;
 
+import com.example.asad.tictoe.Controller;
+
 /**
  * Created by osama on 9/19/16.
  * the backend handler for getting moves
@@ -9,20 +11,37 @@ import android.app.Activity;
 public class UIController {
 
     ServerConnector connector;
-    public UIController(Activity act){
+    Controller manageInterface;
+    private int player;
+    private Boolean gameStatus;
+    public UIController(Activity act,Controller con){
         connector=new ServerConnector();
         connector.setActivity(act);
+        //try to start match
+        connector.arrangeMatch();
+
+        manageInterface=con;
     }
     public void wins(int i) {
     }
 
     public void setGameStatus(boolean b) {
+        this.gameStatus=b;
+        check();
+    }
+
+    private void check() {
+        if(gameStatus){
+            manageInterface.setEnable();
+        }
     }
 
     public void drawBox(int box) {
     }
 
     public void setPlayer(int player) {
+        this.player=player;
+        manageInterface.setPlayerID(player);
     }
 
     public void otherPlayerQuit() {
